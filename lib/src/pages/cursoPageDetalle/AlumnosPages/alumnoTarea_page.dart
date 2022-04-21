4b972c32ba8fc05b2f74ded8cd1218cc0d954da5
Juan Pablo Rizo Quintero <penguin424@Mac-mini-de-Juan.local>
2022-04-21 13:57:03 -0500

    adios mundo

--- lib/src/pages/cursoPageDetalle/AlumnosPages/alumnoTarea_page.dart
+++ lib/src/pages/cursoPageDetalle/AlumnosPages/alumnoTarea_page.dart
@@ -6,7 +6,9 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:http/http.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:loading_overlay/loading_overlay.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:reproductor/src/models/Tarea.dart';
 import 'package:reproductor/src/utils/PrefsSIngle.dart';
 import 'package:universal_html/html.dart' as html;
@@ -89,6 +91,7 @@ class AlumnoTareaPage extends HookWidget {
                     height: 40.0,
                   ),
                   _loadFiles(_archivos),
+                  _loadImagesFromGallery(_archivos, context),
                   SizedBox(
                     height: MediaQuery.of(context).size.height / 4.5,
                   ),
@@ -104,6 +107,65 @@ class AlumnoTareaPage extends HookWidget {
               ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  _loadImagesFromGallery(
+    ValueNotifier<List<MultipartFile>> _archivos,
+    BuildContext context,
+  ) {
+    return ElevatedButton(
+      onPressed: () async {
+        try {
+          final permiso = await Permission.photos.request();
+
+          if (permiso.isGranted) {
+            final picker = ImagePicker();
+            final image = await picker.pickMultiImage();
+
+            if (image != null) {
+              final dataF = image.map((e) async {
+                final bytes = await e.readAsBytes();
+
+                return {
+                  'bytes': bytes.toList(),
+                  'name': e.name,
+                };
+              }).toList();
+
+              final data = await Future.wait(dataF);
+
+              print(data);
+
+              List<MultipartFile> files = data.map((e) {
+                return MultipartFile.fromBytes(
+                  'file',
+                  e['bytes'] as List<int>,
+                  filename: e['name'].toString(),
+                );
+              }).toList();
+
+              print(files);
+
+              _archivos.value = [..._archivos.value, ...files];
+            }
+          } else {
+            _createWindowPermissionHnadler(
+              context,
+              'PERMISO DE GALERIA',
+              'El acceso a la galeria es por si quieres adjuntar una imagen o archivo desde tu galeria o gestor de archivos para poder usarla al entregar tu tarea al maestro, enviarle un mensaje con imagen o poner una foto de perfil',
+            );
+          }
+        } catch (e) {
+          print(e);
+        }
+      },
+      child: Text(
+        'Subir Imagen de galeria',
+        style: TextStyle(
+          color: Colors.white,
         ),
       ),
     );
@@ -607,7 +669,7 @@ class AlumnoTareaPage extends HookWidget {
 
             print(files);
 
-            _archivos.value = files;
+            _archivos.value = [..._archivos.value, ...files];
           } else {
             List<Map<String, dynamic>> data = result.files.map((e) {
               return {
@@ -624,21 +686,53 @@ class AlumnoTareaPage extends HookWidget {
               );
             }).toList();
 
-            List<MultipartFile> filesArr = [];
+            List<MultipartFile> filesArr = await Future.wait(files);
 
-            files.forEach((element) {
-              element.then((value) {
-                filesArr.add(value);
-              });
-            });
+            print(filesArr);
 
-            _archivos.value = filesArr;
+            _archivos.value = [..._archivos.value, ...filesArr];
           }
         } else {
           // User canceled the picker
         }
       },
       child: Text('SUBIR ARCHIVOS'),
+    );
+  }
+
+  _createWindowPermissionHnadler(
+    BuildContext context,
+    String title,
+    String content,
+  ) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 20,
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        content: Text(
+          content,
+          style: TextStyle(fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Denegar'),
+          ),
+          ElevatedButton(
+            onPressed: () => openAppSettings(),
+            child: Text('Permitir desde configutacion'),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/src/pages/cursoPageDetalle/MestrosPages/tareaCrear_page.dart
+++ lib/src/pages/cursoPageDetalle/MestrosPages/tareaCrear_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:http/http.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:reproductor/src/models/Clase_model.dart';
 import 'package:reproductor/src/utils/Http.dart';
 import 'package:reproductor/src/utils/PrefsSIngle.dart';
@@ -80,6 +82,7 @@ class TareaCrear extends HookWidget {
                   height: 40.0,
                 ),
                 _loadFiles(_archivos),
+                _loadImagesFromGallery(_archivos, context),
                 SizedBox(
                   height: MediaQuery.of(context).size.height / 4.5,
                 ),
@@ -395,6 +398,65 @@ class TareaCrear extends HookWidget {
     );
   }
 
+  _loadImagesFromGallery(
+    ValueNotifier<List<MultipartFile>> _archivos,
+    BuildContext context,
+  ) {
+    return ElevatedButton(
+      onPressed: () async {
+        try {
+          final permiso = await Permission.photos.request();
+
+          if (permiso.isGranted) {
+            final picker = ImagePicker();
+            final image = await picker.pickMultiImage();
+
+            if (image != null) {
+              final dataF = image.map((e) async {
+                final bytes = await e.readAsBytes();
+
+                return {
+                  'bytes': bytes.toList(),
+                  'name': e.name,
+                };
+              }).toList();
+
+              final data = await Future.wait(dataF);
+
+              print(data);
+
+              List<MultipartFile> files = data.map((e) {
+                return MultipartFile.fromBytes(
+                  'file',
+                  e['bytes'] as List<int>,
+                  filename: e['name'].toString(),
+                );
+              }).toList();
+
+              print(files);
+
+              _archivos.value = [..._archivos.value, ...files];
+            }
+          } else {
+            _createWindowPermissionHnadler(
+              context,
+              'PERMISO DE GALERIA',
+              'El acceso a la galeria es por si quieres adjuntar una imagen o archivo desde tu galeria o gestor de archivos para poder usarla al entregar tu tarea al maestro, enviarle un mensaje con imagen o poner una foto de perfil',
+            );
+          }
+        } catch (e) {
+          print(e);
+        }
+      },
+      child: Text(
+        'Subir Imagen de galeria',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
   ElevatedButton _loadFiles(ValueNotifier<List<MultipartFile>> _archivos) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
@@ -430,7 +492,7 @@ class TareaCrear extends HookWidget {
 
             print(files);
 
-            _archivos.value = files;
+            _archivos.value = [..._archivos.value, ...files];
           } else {
             List<Map<String, dynamic>> data = result.files.map((e) {
               return {
@@ -447,21 +509,53 @@ class TareaCrear extends HookWidget {
               );
             }).toList();
 
-            List<MultipartFile> filesArr = [];
+            List<MultipartFile> filesArr = await Future.wait(files);
 
-            files.forEach((element) {
-              element.then((value) {
-                filesArr.add(value);
-              });
-            });
+            print(filesArr);
 
-            _archivos.value = filesArr;
+            _archivos.value = [..._archivos.value, ...filesArr];
           }
         } else {
           // User canceled the picker
         }
       },
       child: Text('SUBIR ARCHIVOS'),
+    );
+  }
+
+  _createWindowPermissionHnadler(
+    BuildContext context,
+    String title,
+    String content,
+  ) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 20,
+            color: Theme.of(context).colorScheme.primary,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        content: Text(
+          content,
+          style: TextStyle(fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('Denegar'),
+          ),
+          ElevatedButton(
+            onPressed: () => openAppSettings(),
+            child: Text('Permitir desde configutacion'),
+          ),
+        ],
+      ),
     );
   }
 }
